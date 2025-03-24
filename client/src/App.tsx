ede4606badf51be33f92c04fd1c3beb88b28ea13
이재현 <angle0918@naver.com>
2025-03-24 15:34:12 +0900

    [S12P21A402-166] feat: Set up color and text style guide

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import Router from './routes/Router';
+import './index.css'
 
 function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/dashboard" element={<div>Dashboard</div>} />
-          <Route path="/login" element={<div>Login</div>} />
-          <Route path="/signup" element={<div>Sign Up</div>} />
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </div>
-    </Router>
-  )
+  return <Router />;
 }
 
-export default App
+export default App;

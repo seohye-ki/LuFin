26c42973e7ddeb68c3859aeb31a1f3920538efa9
양은서 <dmstj4871@naver.com>
2025-03-24 16:34:25 +0900

    [S12P21A402-114] feat: Design and create Account entity

---
+++ server/src/main/java/com/lufin/server/account/domain/AutoTransfer.java
@@ -0,0 +1,43 @@
+package com.lufin.server.account.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class AutoTransfer {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Account from;
+
+	private String toAccountNumber;
+
+	private Integer amount;
+
+	private LocalDate startDate;
+
+	private LocalDate endDate;
+
+	private boolean active;
+
+	@Enumerated(EnumType.STRING)
+	private DayOfWeek dayOfWeek; // 납입 요일 (예: 월요일마다)
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+}
---
+++ server/src/main/java/com/lufin/server/account/domain/TransactionHistory.java
@@ -0,0 +1,104 @@
+package com.lufin.server.account.domain;
+
+import java.time.LocalDateTime;
+
+import com.lufin.server.common.constants.HistoryStatus;
+import com.lufin.server.member.domain.Member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TransactionHistory {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Account fromAccount;
+
+	private String toAccountNumber;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member executor;
+
+	@Enumerated(EnumType.STRING)
+	private TransactionType transactionType;
+
+	@Enumerated(EnumType.STRING)
+	private TransactionSourceType sourceType;
+
+	private Integer amount;
+
+	private Integer balanceAfter;
+
+	@Enumerated(EnumType.STRING)
+	private HistoryStatus status;
+
+	private String description;
+
+	private LocalDateTime createdAt;
+
+	private TransactionHistory(
+		Account fromAccount,
+		String toAccountNumber,
+		Member executor,
+		int amount,
+		int balanceAfter,
+		TransactionType type,
+		HistoryStatus status,
+		String description,
+		TransactionSourceType sourceType
+	) {
+		this.fromAccount = fromAccount;
+		this.toAccountNumber = toAccountNumber;
+		this.executor = executor;
+		this.amount = amount;
+		this.balanceAfter = balanceAfter;
+		this.transactionType = type;
+		this.status = status;
+		this.description = description;
+		this.sourceType = sourceType;
+	}
+
+	public static TransactionHistory create(
+		Account fromAccount,
+		String toAccountNumber,
+		Member executor,
+		int amount,
+		int balanceAfter,
+		TransactionType type,
+		HistoryStatus status,
+		String description,
+		TransactionSourceType sourceType
+	) {
+		return new TransactionHistory(
+			fromAccount,
+			toAccountNumber,
+			executor,
+			amount,
+			balanceAfter,
+			type,
+			status,
+			description,
+			sourceType
+		);
+	}
+
+	@PrePersist
+	public void onCreate() {
+		this.createdAt = LocalDateTime.now();
+	}
+}

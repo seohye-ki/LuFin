1692670a7b1a7a1ad3790d1bfb75fe98e5e1f5b3
양은서 <dmstj4871@naver.com>
2025-03-25 14:25:56 +0900

    [S12P21A402-116] feat: Update Account entity for auto account creation

--- server/src/main/java/com/lufin/server/account/domain/Account.java
+++ server/src/main/java/com/lufin/server/account/domain/Account.java
@@ -4,11 +4,14 @@ import static com.lufin.server.common.constants.ErrorCode.*;
 
 import java.time.LocalDateTime;
 
+import com.lufin.server.classroom.domain.Classroom;
 import com.lufin.server.common.exception.BusinessException;
 import com.lufin.server.member.domain.Member;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,18 +21,29 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "account", uniqueConstraints = @UniqueConstraint(columnNames = "accountNumber"))
+@Table(name = "accounts", uniqueConstraints = @UniqueConstraint(columnNames = "accountNumber"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Column(nullable = false, unique = true, length = 30)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "classroom_id")
+	private Classroom classroom;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "account_type", nullable = false)
+	private AccountType type; // 예: DEPOSIT, LOAN, CLASSROOM
+
+	@Column(nullable = false, unique = true, length = 15)
 	private String accountNumber;
 
 	@Column(nullable = false)
@@ -40,12 +54,32 @@ public class Account {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	// TODO: 클래스 계좌일 경우 추가
-
 	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
 	private LocalDateTime closedAt;
+
+	private Account(String accountNumber, AccountType type, Member member) {
+		this.accountNumber = accountNumber;
+		this.member = member;
+		this.type = type;
+		onCreate();
+	}
+
+	private Account(String accountNumber, AccountType type, Classroom classroom) {
+		this.accountNumber = accountNumber;
+		this.classroom = classroom;
+		this.type = type;
+		onCreate();
+	}
+
+	public static Account create(String accountNumber, Member member) {
+		return new Account(accountNumber, AccountType.DEPOSIT, member);
+	}
+
+	public static Account createClassAccount(String accountNumber, Classroom classroom) {
+		return new Account(accountNumber, AccountType.CLASSROOM, classroom);
+	}
 
 	@PrePersist
 	public void onCreate() {

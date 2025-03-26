e9702d28c139302fe0c7da0f361356e2315be0cb
양은서 <dmstj4871@naver.com>
2025-03-26 16:51:58 +0900

    [S12P21A402-112] fix: Change HTTP method to POST for class creation API

--- server/src/main/java/com/lufin/server/classroom/controller/ClassroomController.java
+++ server/src/main/java/com/lufin/server/classroom/controller/ClassroomController.java
@@ -1,7 +1,7 @@
 package com.lufin.server.classroom.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ public class ClassroomController {
 	private final ClassroomService classroomService;
 
 	// 클래스 생성
-	@GetMapping
+	@PostMapping
 	ResponseEntity<ApiResponse<ClassResponse>> createClassroom(ClassRequest classRequest) {
 		Member currentMember = UserContext.get();
 		ClassResponse result = classroomService.createClassroom(classRequest, currentMember);

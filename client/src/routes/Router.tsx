a28100bae1041525c1495e7378367525ff6df717
Minju Choi <minjumost@naver.com>
2025-03-28 09:26:50 +0900

    [S12P21A402-208] feat: add Route for Shop page

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -5,6 +5,7 @@ import { paths } from './paths';
 import Home from '../pages/common/Home/Home';
 import PrivateRoute from './PrivateRoute';
 import CommonComponents from '../pages/common/Home/CommonComponents';
+import Shop from '../pages/student/Shop/Shop';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
       <Routes>
         <Route path={paths.HOME} element={<Home />} />
         <Route path={paths.COMMON_COMPONENTS} element={<CommonComponents />} />
+        <Route path={paths.SHOP} element={<Shop />} />
         {/* 인증이 필요한 라우트들은 PrivateRoute로 감싸기 */}
         <Route element={<PrivateRoute />}>{/* 여기에 인증이 필요한 라우트들 추가 */}</Route>
       </Routes>
--- client/src/routes/paths.ts
+++ client/src/routes/paths.ts
@@ -2,4 +2,5 @@ export const paths = {
   HOME: '/',
   LOGIN: '/login',
   COMMON_COMPONENTS: '/common-components',
+  SHOP: '/shop',
 } as const;

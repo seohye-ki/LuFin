a1308fdb34372cbe12b73d5ef752b25e19596b9a
양은서 <dmstj4871@naver.com>
2025-03-28 17:09:02 +0900

    [S12P21A402-103] feat: Add login API

---
+++ server/src/main/java/com/lufin/server/member/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.lufin.server.member.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lufin.server.common.dto.ApiResponse;
+import com.lufin.server.member.dto.LoginRequest;
+import com.lufin.server.member.dto.LoginResponse;
+import com.lufin.server.member.service.LoginService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/lufin/auth")
+public class AuthController {
+
+	private final LoginService loginService;
+
+	@PostMapping("login")
+	public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
+
+		LoginResponse response = loginService.login(request.email(), request.password());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Cache-Control", "no-store");
+		headers.add("Pragma", "no-cache");
+
+		return ResponseEntity.ok().headers(headers).body(ApiResponse.success(response));
+	}
+}

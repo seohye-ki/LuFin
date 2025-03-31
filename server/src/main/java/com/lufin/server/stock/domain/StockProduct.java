c4fa6be9aaab2def62967a5406554e9a00a04352
whghdrbs1002 <whghdrbs1002@gmail.com>
2025-03-31 13:15:56 +0900

    [S12P21A402-161] feat: Create stock news entity

--- server/src/main/java/com/lufin/server/stock/domain/StockProduct.java
+++ server/src/main/java/com/lufin/server/stock/domain/StockProduct.java
@@ -1,12 +1,16 @@
 package com.lufin.server.stock.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -26,11 +30,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StockProduct {
 	//TODO: 다른 테이블과 연관관계 설정
+	@Builder.Default
+	@OneToMany(mappedBy = "stockProduct", cascade = CascadeType.ALL, orphanRemoval = true)
+	private final List<StockNews> news = new ArrayList<>();
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "stock_product_id", nullable = false)
-	private Long id;
+	private int id;
 
 	@Column(name = "name", nullable = false)
 	@Max(value = 50)
@@ -75,6 +82,44 @@ public class StockProduct {
 			.initialPrice(initialPrice)
 			.currentPrice(currentPrice)
 			.build();
+	}
+
+	/* 연관관계 메서드 */
+
+	/**
+	 * 주식 공시 정보(StockNews)와의 양방향 연관관계를 위한 메서드
+	 * @param news
+	 */
+	public void addNews(StockNews news) {
+		// 중복 체크 후 없으면 추가
+		if (!this.news.contains(news)) {
+			this.news.add(news);
+		}
+
+		// 양방향 관계 설정 (무한 루프 방지 조건)
+		if (news.getStockProduct() != this) {
+			news.setStockProduct(this);
+		}
+	}
+
+	// 무한 루프 방지를 위해 contains()를 사용하면서 id를 기준으로 체크하기 위해 equals override
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+			
+		StockProduct that = (StockProduct)o;
+		return id == that.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Integer.hashCode(id);
 	}
 
 }

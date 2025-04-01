558aabadfa834270d64a8ba2cce74926446508c0
whghdrbs1002 <whghdrbs1002@gmail.com>
2025-04-01 13:46:45 +0900

    [S12P21A402-160] feat: Implement repository for getting stock news

--- server/src/main/java/com/lufin/server/stock/repository/StockNewsRepositoryImpl.java
+++ server/src/main/java/com/lufin/server/stock/repository/StockNewsRepositoryImpl.java
@@ -4,20 +4,50 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.lufin.server.stock.domain.QStockNews;
+import com.lufin.server.stock.dto.QStockNewsResponseDto_NewsInfoDto;
 import com.lufin.server.stock.dto.StockNewsResponseDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class StockNewsRepositoryImpl implements StockNewsRepository {
+	private final JPAQueryFactory queryFactory;
+
 	@Override
 	public List<StockNewsResponseDto.NewsInfoDto> getAllNews() {
-		return List.of();
+		QStockNews stockNews = QStockNews.stockNews;
+
+		List<StockNewsResponseDto.NewsInfoDto> result = queryFactory
+			.select(new QStockNewsResponseDto_NewsInfoDto(
+				stockNews.id,
+				stockNews.content,
+				stockNews.createdAt,
+				stockNews.updatedAt
+			))
+			.from(stockNews)
+			.fetch();
+
+		return result;
 	}
 
 	@Override
 	public StockNewsResponseDto.NewsInfoDto getNewsByNewsId(Integer newsId) {
-		return null;
+		QStockNews stockNews = QStockNews.stockNews;
+
+		StockNewsResponseDto.NewsInfoDto result = queryFactory
+			.select(new QStockNewsResponseDto_NewsInfoDto(
+				stockNews.id,
+				stockNews.content,
+				stockNews.createdAt,
+				stockNews.updatedAt
+			))
+			.from(stockNews)
+			.where(stockNews.id.eq(newsId))
+			.fetchOne();
+
+		return result;
 	}
 }

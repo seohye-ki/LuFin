d199ad217b047abdb67c270da6292b74058b2433
양은서 <dmstj4871@naver.com>
2025-04-03 21:31:57 +0900

    [S12P21A402-280] chore: Apply CORS configuration

--- server/src/main/java/com/lufin/server/config/WebConfig.java
+++ server/src/main/java/com/lufin/server/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.lufin.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
 			.excludePathPatterns("/api/v1/lufin/auth/login", "/api/v1/lufin/register/**", "/css/**", "/js/**",
 				"/favicon.ico",
 				"/api/test-login", "/app/**", "/ws/**"); // 예외 경로
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://j12a402.p.ssafy.io:5173", "http://localhost:5173")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS 포함
+			.allowCredentials(true) // 쿠키 인증 허용
+			.allowedHeaders("*") // 모든 헤더 허용
+			.maxAge(3600); // preflight 요청 캐싱 시간
 	}
 }

93065346620c934d0f3d67eeaabd4eb9783915cc
seohyeon.shkim <seohyeon.shkim@gmail.com>
2025-04-03 10:53:02 +0900

    [S12P21A402-246] feat: Update loan DTO

--- server/src/main/java/com/lufin/server/loan/repository/LoanProductRepository.java
+++ server/src/main/java/com/lufin/server/loan/repository/LoanProductRepository.java
@@ -1,5 +1,7 @@
 package com.lufin.server.loan.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.lufin.server.loan.domain.LoanProduct;
 
 @Repository
 public interface LoanProductRepository extends JpaRepository<LoanProduct, Integer> {
+
+	List<LoanProduct> findByCreditRank(Integer creditRank);
 }

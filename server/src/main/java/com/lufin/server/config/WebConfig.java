94c9694e4fb058f1c4ae3c225985f3b4c49683be
양은서 <dmstj4871@naver.com>
2025-04-04 13:49:39 +0900

    [S12P21A402-280] chore: Add 127.0.0.1 to CORS allowed origins

--- server/src/main/java/com/lufin/server/config/WebConfig.java
+++ server/src/main/java/com/lufin/server/config/WebConfig.java
@@ -29,7 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://j12a402.p.ssafy.io:5173", "http://localhost:5173")
+			.allowedOrigins("https://j12a402.p.ssafy.io:5173", "http://j12a402.p.ssafy.io:5173",
+				"http://localhost:5173", "http://127.0.0.1:5173")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS 포함
 			.allowCredentials(true) // 쿠키 인증 허용
 			.allowedHeaders("*") // 모든 헤더 허용

2fbd6cce5de9167fcfc6b526d79defcfe0552c61
Minju Choi <minjumost@naver.com>
2025-04-09 23:09:04 +0900

    [S12P21A402-326 fix: hide stock menu for teacher

--- client/src/components/Sidebar/Menu.tsx
+++ client/src/components/Sidebar/Menu.tsx
@@ -1,3 +1,4 @@
+import useAuthStore from '../../libs/store/authStore';
 import useClassroomStore from '../../libs/store/classroomStore';
 import MenuItem, { MenuType } from './MenuItem';
 
@@ -5,12 +6,16 @@ const menuList: MenuType[] = ['dashboard', 'mission', 'shop', 'stock', 'loan'];
 
 const Menu = () => {
   const { currentClassId } = useClassroomStore();
+  const { userRole } = useAuthStore();
+
+  const filteredMenu = menuList.filter((type) => type !== 'stock' || userRole !== 'TEACHER');
+
   return (
     currentClassId && (
       <div className='flex flex-col gap-2'>
         <p className='text-c1 font-semibold text-dark-grey'>메뉴</p>
         <div className='h-full flex flex-col gap-2'>
-          {menuList.map((type) => (
+          {filteredMenu.map((type) => (
             <MenuItem key={type} type={type} />
           ))}
         </div>

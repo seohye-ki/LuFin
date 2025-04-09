b2f13e72b6f30bf00ad6102942155e6fef2806c8
Minju Choi <minjumost@naver.com>
2025-04-09 23:47:31 +0900

    [S12P21A402-326 fix: change logout logic

--- client/src/pages/common/Home/components/Header.tsx
+++ client/src/pages/common/Home/components/Header.tsx
@@ -3,10 +3,39 @@ import { paths } from '../../../../routes/paths';
 import logo from '../../../../assets/svgs/logo.svg';
 import Button from '../../../../components/Button/Button';
 import useAuthStore from '../../../../libs/store/authStore';
+import { hideGlobalAlert, showGlobalAlert } from '../../../../libs/store/alertStore';
 
 export function Header() {
   const { isAuthenticated, logout } = useAuthStore();
 
+  const handleLogout = () => {
+    showGlobalAlert(
+      '로그아웃 하시겠습니까?',
+      null,
+      '',
+      'warning',
+      {
+        label: '확인',
+        onClick: () => {
+          showGlobalAlert(
+            '로그아웃 했어요.',
+            null,
+            '확인을 누르면 로그인 페이지로 이동합니다.',
+            'info',
+            {
+              label: '확인',
+              onClick: () => {
+                logout(); // 실제 상태 초기화는 여기서
+                hideGlobalAlert();
+                window.location.href = '/login'; // 진짜 리다이렉션도 여기서
+              },
+            },
+          );
+        },
+      },
+      { label: '취소', onClick: hideGlobalAlert },
+    );
+  };
   return (
     <header className='absolute inset-x-0 top-0 z-50'>
       <nav className='flex items-center justify-between p-6 lg:px-8' aria-label='Global'>
@@ -18,7 +47,7 @@ export function Header() {
         </div>
         <div className='flex flex-1 justify-end'>
           {isAuthenticated ? (
-            <Button color='info' onClick={logout}>
+            <Button color='info' onClick={handleLogout}>
               로그아웃
             </Button>
           ) : (
